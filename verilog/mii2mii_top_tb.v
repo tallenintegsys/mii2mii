6e67abf4eddcdf28475869b8d7a79ac1bc92d55c
Tim Allen <tallen@integsys.biz>
2021-12-21 15:38:13 -0500

    it's as verified as it's gonna get

--- verilog/mii2mii_top_tb.v
+++ verilog/mii2mii_top_tb.v
@@ -39,6 +39,18 @@ localparam crc = 32'hfb029064; //we can ignore this
 
 integer i;
 reg [7:0]c;
+integer step = 0;
+
+task chk (input integer s, input [3:0] value);
+	begin
+		if (step == s) begin
+			if (miiO_d != value) begin
+				$display("step: %d got: %b", step, miiO_d);
+				$finish;
+			end
+		end
+	end
+endtask
 
 initial begin
 	$dumpfile("MIIulatorTop.vcd");
@@ -119,8 +131,49 @@ always #22 miiO_clk = !miiO_clk;
 
 always @(posedge miiO_clk) begin
 	if (miiO_en) begin
-		$display("got: %b", miiO_d);
+		if (step < 17) begin
+			if (miiO_d != 4'b1010) begin
+				$display("step: %d got: %b", step, miiO_d);
+				$finish;
+			end
+		end
+		//sof
+		chk(17, 4'b1011);
+		//dst
+		chk(18, {dst[40],dst[41],dst[42],dst[43]});
+		chk(19, {dst[44],dst[45],dst[46],dst[47]});
+		chk(20, {dst[32],dst[33],dst[34],dst[35]});
+		chk(21, {dst[36],dst[37],dst[38],dst[39]});
+		chk(22, {dst[24],dst[25],dst[26],dst[27]});
+		chk(23, {dst[28],dst[29],dst[30],dst[31]});
+		chk(24, {dst[16],dst[17],dst[18],dst[19]});
+		chk(25, {dst[20],dst[21],dst[22],dst[23]});
+		chk(26, {dst[8],dst[9],dst[10],dst[11]});
+		chk(27, {dst[12],dst[13],dst[14],dst[15]});
+		chk(28, {dst[0],dst[1],dst[2],dst[3]});
+		chk(29, {dst[4],dst[5],dst[6],dst[7]});
+		//src
+		chk(30, {src[40],src[41],src[42],src[43]});
+		chk(31, {src[44],src[45],src[46],src[47]});
+		chk(32, {src[32],src[33],src[34],src[35]});
+		chk(33, {src[36],src[37],src[38],src[39]});
+		chk(34, {src[24],src[25],src[26],src[27]});
+		chk(35, {src[28],src[29],src[30],src[31]});
+		chk(36, {src[16],src[17],src[18],src[19]});
+		chk(37, {src[20],src[21],src[22],src[23]});
+		chk(38, {src[8],src[9],src[10],src[11]});
+		chk(39, {src[12],src[13],src[14],src[15]});
+		chk(40, {src[0],src[1],src[2],src[3]});
+		chk(41, {src[4],src[5],src[6],src[7]});
+		//type
+		chk(42, {type[8],type[9],type[10],type[11]});
+		chk(43, {type[12],type[13],type[14],type[15]});
+		chk(44, {type[0],type[1],type[2],type[3]});
+		chk(45, {type[4],type[5],type[6],type[7]});
+		//payload
+		//good enough already!
 	end
+	step <= step + 1;
 end // always
 
 mii2mii uut(
